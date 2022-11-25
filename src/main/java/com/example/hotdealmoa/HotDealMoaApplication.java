f662cf538be18688bf018fe6c8ad5ffc075147aa
yht0827 <yangheetae0827@naver.com>
2022-11-26 00:23:10 +0900

    init commit

---
+++ src/main/java/com/example/hotdealmoa/HotDealMoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotdealmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotDealMoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotDealMoaApplication.class, args);
+	}
+
+}
