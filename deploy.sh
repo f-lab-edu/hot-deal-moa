05f57af9f1ad3d1992e719b0c57fc58aa76dd7a5
yht0827 <yangheetae0827@naver.com>
2022-12-16 12:42:45 +0900

    [#8] CD 구성 - 2
    - deploy.sh 스크립트 추가
    - 배포 테스트용 api 추가

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "> now ing app pid find!"
+CURRENT_PID=$(pgrep -f hot-deal-moa)
+echo "$CURRENT_PID"
+if [ -z $CURRENT_PID ]; then
+  echo "> no ing app."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -9 $CURRENT_PID
+  sleep 3
+fi
+echo "> new app deploy"
+
+cd /hotdealmoa/build/libs/
+JAR_NAME=$(ls | grep 'gist-competition' | tail -n 1)
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar -Duser.timezone=Asia/Seoul $JAR_NAME 1>nohup/stdout.txt 2>nohup/stderr.txt &
+sleep 2
