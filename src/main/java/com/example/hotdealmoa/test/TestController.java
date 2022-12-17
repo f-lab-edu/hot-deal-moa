8f4f25ad894708f551e0e760449bd3799d9d9517
yht0827 <yangheetae0827@naver.com>
2022-12-17 13:14:05 +0900

    [#5] Spring Rest Doc 추가
    - Spring Rest Doc 의존성 추가
    - Rest Doc 테스트 api 및 문서 추가

--- src/main/java/com/example/hotdealmoa/test/TestController.java
+++ src/main/java/com/example/hotdealmoa/test/TestController.java
@@ -2,7 +2,10 @@ package com.example.hotdealmoa.test;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +28,18 @@ public class TestController {
 		}
 
 		return "server: " + ip + " deploy test version 1";
+	}
+
+	@GetMapping("/restDoc")
+	public ResponseEntity<Map<String, Object>> testRestDoc() {
+
+		Map<String, Object> map = new HashMap<>();
+
+		map.put("name", "aa");
+		map.put("message", "hello!");
+
+		return ResponseEntity.ok()
+			.body(map);
 	}
 
 }
---
+++ src/test/java/com/example/hotdealmoa/common/config/RestDocTest.java
@@ -0,0 +1,51 @@
+package com.example.hotdealmoa.common.config;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.example.hotdealmoa.test.TestController;
+
+@ExtendWith(RestDocumentationExtension.class)
+@WebMvcTest(TestController.class)
+public class RestDocTest {
+
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+			.apply(documentationConfiguration(restDocumentation))
+			.build();
+	}
+
+	@Test
+	@DisplayName("Spring RestDoc 문서 테스트")
+	void restDocTest() throws Exception {
+
+		// then
+		mockMvc.perform(get("/test/restDoc"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andDo(document("test", responseFields(
+				fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+				fieldWithPath("message").type(JsonFieldType.STRING).description("메세지")
+			)));
+
+	}
+
+}
