8dcf84c91e5e8c6d7be6d6d0d43dd48c646075dd
yht0827 <yangheetae0827@naver.com>
2022-12-20 11:43:33 +0900

    [#15] CI / CD workflow 수정
    - 배포 시 디렉토리 및 jar파일 명 수정

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "> now ing app pid find!"
-CURRENT_PID=$(pgrep -f hot-deal-moa)
+CURRENT_PID=$(pgrep -f HotDealMoaApp)
 echo "$CURRENT_PID"
 if [ -z $CURRENT_PID ]; then
   echo "> no ing app."
@@ -11,9 +11,7 @@ else
 fi
 echo "> new app deploy"
 
-cd /hotdealmoa/build/libs/
-JAR_NAME=$(ls | grep 'SNAPSHOT.jar')
-echo "> JAR Name: $JAR_NAME"
+cd /hotdealmoa
 
-nohup java -jar $JASYPT_PASSWORD -Duser.timezone=Asia/Seoul $JAR_NAME 1>/hotdealmoa/application.log 2>&1 &
+nohup java -jar $JASYPT_PASSWORD -Duser.timezone=Asia/Seoul HotDealMoaApp.jar 1>/hotdealmoa/application.log 2>&1 &
 sleep 2
