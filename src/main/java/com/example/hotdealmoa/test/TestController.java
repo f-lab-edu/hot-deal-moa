a007adcc5e5df3e3a654a3d0e03a6e4e3cc81a24
yht0827 <yangheetae0827@naver.com>
2022-12-21 14:39:17 +0900

    [#13] 회원가입 구현
    - mapstruct 의존성 추가
    - response 테스트용 url 추가

--- src/main/java/com/example/hotdealmoa/test/TestController.java
+++ src/main/java/com/example/hotdealmoa/test/TestController.java
@@ -1,8 +1,12 @@
 package com.example.hotdealmoa.test;
 
+import static com.example.hotdealmoa.global.common.response.ResponseHandler.*;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.hotdealmoa.global.common.response.ResponseResult;
+import com.example.hotdealmoa.global.exception.CustomException;
+import com.example.hotdealmoa.global.exception.ErrorCode;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -42,4 +53,36 @@ public class TestController {
 			.body(map);
 	}
 
+	@GetMapping("/success")
+	public ResponseResult<?> testSuccess() {
+		List<TestMember> list = new ArrayList<>();
+
+		for (int i = 0; i < 5; i++) {
+			list.add(new TestMember("aa", i));
+		}
+
+		return success(list);
+	}
+
+	@GetMapping("/success1")
+	public ResponseResult<?> testSuccess1() {
+		return success();
+	}
+
+	@GetMapping("/error")
+	public void testError() {
+		throw new CustomException(ErrorCode.BAD_REQUEST);
+	}
+
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@Getter
+	public static class TestMember {
+		private String name;
+		private Integer age;
+
+		public TestMember(String name, Integer age) {
+			this.name = name;
+			this.age = age;
+		}
+	}
 }
