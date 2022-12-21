a8e4e74c25e7a145e570fb35fdf6ff54c21d71ef
yht0827 <yangheetae0827@naver.com>
2022-12-21 14:34:21 +0900

    [#13] 회원가입 구현
    - 에러 수정

--- src/main/java/com/example/hotdealmoa/member/domain/Member.java
+++ src/main/java/com/example/hotdealmoa/member/domain/Member.java
@@ -1,6 +1,4 @@
 package com.example.hotdealmoa.member.domain;
 
-import com.example.hotdealmoa.global.config.common.BaseTimeEntity;
-
-public class Member extends BaseTimeEntity {
+public class Member {
 }
