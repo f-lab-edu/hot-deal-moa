f712772fa5d7d83d742cfa05d7334849548a56d6
yht0827 <yangheetae0827@naver.com>
2022-12-31 15:54:58 +0900

    [#14] 로그인 구현
    - 로그인 체크 에러 수정

--- src/main/java/com/example/hotdealmoa/global/common/LoginUserArgumentResolver.java
+++ src/main/java/com/example/hotdealmoa/global/common/LoginUserArgumentResolver.java
@@ -1,6 +1,5 @@
 package com.example.hotdealmoa.global.common;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -9,8 +8,6 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 import com.example.hotdealmoa.global.common.annotation.CurrentUser;
-import com.example.hotdealmoa.global.exception.CustomException;
-import com.example.hotdealmoa.global.exception.ErrorCode;
 import com.example.hotdealmoa.member.service.LoginService;
 
 import lombok.RequiredArgsConstructor;
@@ -30,13 +27,6 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
 		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
 
-		String currentUser = loginService.getCurrentUser();
-
-		// 현재 유저가 로그인되지 않은 경우
-		if (StringUtils.isEmpty(currentUser)) {
-			throw new CustomException(ErrorCode.NOT_LOGIN);
-		}
-
-		return currentUser;
+		return loginService.getCurrentUser();
 	}
 }
